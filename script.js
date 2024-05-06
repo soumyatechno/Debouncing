998c00084c2a59dfbed682b6921fbea705e2bf47
soumya <soumya.chakraborty@hoichoi.tv>
2024-05-06 13:06:01 +0530

    project setup

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Hello World!');
+const input = document.querySelector('input');
+const defaultText = document.getElementById('default');
+const debounceText = document.getElementById('debouce');
+const throttledText = document.getElementById('throttle');
